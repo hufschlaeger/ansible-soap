dff2be5816fe64ce674bf9912871a265467f47f8
Daniel Hufschläger <daniel@hufschlaeger.net>
2025-11-01 19:04:47 +0100

    Reorder import statements and remove redundant shebang padding

--- plugins/modules/soap_request.py
+++ plugins/modules/soap_request.py
@@ -1,10 +1,10 @@
-from __future__ import absolute_import, division, print_function
-
-# !/usr/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2024, Daniel Hufschläger <daniel@hufschlaeger.net>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
